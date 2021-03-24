3acc40d0c01334153824b85b1826262c9b94ec5a
dtrivi <danieltrivinos@gmail.com>
2021-03-24 16:32:41 -0500

    Part 1: Connect a Database to a Spring App - attempted/completed all steps, including through Test It with SQL. Unsure if addressed SQL TASK #1 correctly in queries.sql, so left it commented out.

--- queries.sql
+++ queries.sql
@@ -1,4 +1,11 @@
 ## Part 1: Test it with SQL
+-- create/read/update table? list columns and data found in table or into the table?
+--CREATE TABLE jobs (
+--    id INT,
+--    employer VARCHAR(255),
+--    name VARCHAR(255),
+--    skills VARCHAR(255)
+--);
 
 ## Part 2: Test it with SQL
 
