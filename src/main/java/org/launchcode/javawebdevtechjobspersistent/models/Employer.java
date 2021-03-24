75c87210781eacb85e9685813c80fec4fd22736d
dtrivi <danieltrivinos@gmail.com>
2021-03-24 18:17:18 -0500

    Part 2: Persisting Employers and Skills - attempted/completed through Controllers, step 4 (of 5). Still need to create SkillController and Test It with SQL. Unsure if I addressed the index method in Controllers.2 correctly.

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,24 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    // Unsure of what validation is appropriate, went with validation for a String
+    @NotBlank(message = "Employer location is required.")
+    @Size(min = 2, max = 150, message = "Location must be between 2 and 150 characters.")
+    private String location;
 
+    public Employer() {}
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,16 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
+
+    @NotBlank(message = "Position skill is required.")
+    @Size(min = 2, max = 999, message = "Skill must be described between 2 and 999 characters.")
+    private String skill;
+
+    public Skill() {}
 
 }
